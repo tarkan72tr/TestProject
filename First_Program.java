beb8e0060ba6730ad90fbfdafe757b09930db7ed
Tarkan EREN <tarkan72tr@gmail.com>
2022-06-01 19:54:07 -0400

    first commit

--- First_Program.java
+++ First_Program.java
@@ -2,6 +2,7 @@ public class First_Program {
 	public static void main(String[] args)  {
 	  System.out.println("This is my first java program!");
 	  System.out.println("Java is awesome!");
+	  System.out.println("Hello World!");
   }
  
 
